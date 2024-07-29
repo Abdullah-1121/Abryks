95afaf3abfbf75c1157cbd764e90d825d5cba138
muhammad abdullah <muhammad11abdullah21@gmail.com>
2024-07-28 17:36:30 -0700

    Updated the hero section with a new banner design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import img1 from '@/assets/jennifer-marquez-60VYe8pVah8-unsplash-removebg-preview.png'
+import shirts from '@/assets/shirt-mockup-concept-with-plain-clothing.png'
 import img2 from '@/assets/tania-mousinho-4nkHv1jlbOk-unsplash-removebg-preview.png'
 import ImageSlider from '@/components/imageslider'
 const hero = function (){
@@ -9,27 +10,25 @@ const hero = function (){
         { url: img2, alt: "Image Two" },
     ]
     return (
-        <div className='flex min-h-screen w-ful bg-gray-200 md:flex-row flex-col md:order-1 order-2'>
-            <div className='h-screen w-full border-2 border-black flex flex-col p-4 justify-center items-center'>
-                <div className='flex flex-col space-y-4'>
-                    <div className='border-2 border-black'>
-                        <h1 className='text-5xl'>GET UPTO 30% OFF </h1>
-                        <h1 className='text-5xl'>NEW SUMMER ARRIVALS</h1>
-
-                    </div>
-                    <div className='border-2 border-black'>
-                        <button className='px-4 py-2 m-4 bg-red-500 text-white rounded font-semibold '>Shop Now</button>
-                    </div>
-                    <div></div>
-                </div>
-
-            </div>
-            <div className='h-screen w-full border-2 border-black flex flex-col p-4 items-center justify-center md-order-2 order-1 '>
-                <ImageSlider images={IMAGES}></ImageSlider>
-            
-
-            </div>
-        </div>
+        <div className="flex flex-col-reverse md:flex-row items-center border-2 border-gray-300 m-2 rounded-lg justify-between bg-gray-300 p-6 md:p-12">
+      <div className="flex flex-col items-center md:items-start text-center md:text-left md:w-1/2 space-y-4">
+        <h1 className="text-2xl md:text-4xl font-bold">
+          GET UPTO 30% OFF ON NEW ARRIVALS
+        </h1>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          Shop Now
+        </button>
+      </div>
+      <div className="md:w-1/2 mb-6 md:mb-0">
+        <Image
+          src={shirts}
+          alt="Featured Product"
+          width={500}
+          height={500}
+          className="object-cover"
+        />
+      </div>
+    </div>
     )
 
 }
