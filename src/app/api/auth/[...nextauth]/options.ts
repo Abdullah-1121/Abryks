d7a3d4dcc1c1f2ca60686f4e4f116f58865b2bbc
Abdullah-1121 <muhammad11abdullah21@gmail.com>
2024-08-08 00:20:47 -0700

    working on authentication system

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,50 +1,133 @@
-import CredentialsProvider from "next-auth/providers/credentials";
-import NextAuthOptions from 'next-auth'
-import { dbConnect} from '@/lib/dbconnect'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+import { dbConnect } from "@/lib/dbconnect";
 import User from "@/models/User";
-import bcrypt from 'bcryptjs'
-import { signIn } from "@/schemas/signin";
-export const authOptions ={
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+export const authOptions : NextAuthOptions = {
     providers:[
-       CredentialsProvider({
-            id:"credentials",
-            name: 'Credentials',
-            credentials: {
-              email: { label: 'Email', type: 'text' },
-              password: { label: 'Password', type: 'password' },
-            },
-            async authorize(credentials:any):Promise<any>{
-                await dbConnect();
-                try{
-                const user =     await User.findOne({
-                         email: credentials.identifier
-                    })
-                    if(!user){
-                        throw new Error ('No user found with this email')
-                    }
-                    const isPasswordCorrect = await bcrypt.compare(credentials.password , user.password);
-                    if(isPasswordCorrect){
-                        return user
-                    }else{
-                        throw new Error('Passowrd is incorrect')
-                    }
+        CredentialsProvider({
+                        id:"credentials",
+                        name: 'Credentials',
+                        credentials: {
+                          email: { label: 'Email', type: 'text' },
+                          password: { label: 'Password', type: 'password' },
+                        },
+                        async authorize(credentials:any):Promise<any>{
+                            await dbConnect();
+                            try{
+                            const user =     await User.findOne({
+                                     email: credentials.identifier
+                                })
+                                if(!user){
+                                    throw new Error ('No user found with this email')
+                                }
+                                const isPasswordCorrect = await bcrypt.compare(credentials.password , user.password);
+                                if(isPasswordCorrect){
+                                    return user
+                                }else{
+                                    throw new Error('Passowrd is incorrect')
+                                }
+            
+            
+                            }catch(error:any){
+                                throw new Error(error)
+                            }
+            
+                        }
+                    }),
+                    GoogleProvider({
+                        clientId: process.env.CLIENT_ID!,
+                        clientSecret: process.env.CLIENT_SECRET!
+                      }),
 
-
-                }catch(error:any){
-                    throw new Error(error)
-                }
-
-            }
-        }),
-        
     ],
-    pages : {
-        signIn:'/sign-in'
+    callbacks:{
+
+        async jwt({ token, user }) {
+            if (user) {
+              token._id = user._id;
+            }
+            return token
     },
-    session : {
-        strategy:'jwt'
+    async session({ session, token }) {
+        if(token){
+        session.user._id = token._id,
+        session.user.email = token.email
+        }
+        return session
     }
+    
+},
+pages:{
+    signIn:'/sign-in'
+},
+session:{
+    strategy:"jwt"
+},
+
+}
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import NextAuthOptions , { NextAuthOptions } from "next-auth"
+// import NextAuth  from 'next-auth';
+// import { dbConnect} from '@/lib/dbconnect'
+// import User from "@/models/User";
+// import bcrypt from 'bcryptjs'
+// import GoogleProvider from "next-auth/providers/google";
+// import { signIn } from "@/schemas/signin";
+// export const authOptions : NextAuthOptions  = {
+//     providers:[
+//        CredentialsProvider({
+//             id:"credentials",
+//             name: 'Credentials',
+//             credentials: {
+//               email: { label: 'Email', type: 'text' },
+//               password: { label: 'Password', type: 'password' },
+//             },
+//             async authorize(credentials:any):Promise<any>{
+//                 await dbConnect();
+//                 try{
+//                 const user =     await User.findOne({
+//                          email: credentials.identifier
+//                     })
+//                     if(!user){
+//                         throw new Error ('No user found with this email')
+//                     }
+//                     const isPasswordCorrect = await bcrypt.compare(credentials.password , user.password);
+//                     if(isPasswordCorrect){
+//                         return user
+//                     }else{
+//                         throw new Error('Passowrd is incorrect')
+//                     }
+
+
+//                 }catch(error:any){
+//                     throw new Error(error)
+//                 }
+
+//             }
+//         }),
+//         GoogleProvider({
+//             clientId: process.env.CLIENT_ID,
+//             clientSecret: process.env.CLIENT_SECRET
+//           }),
+//     ],
+//     pages : {
+//         signIn:'/sign-in'
+//     },
+//     session : {
+//         strategy:'jwt'
+//     },
+//     callbacks : {
+
+//         async jwt({ token, user }) {
+//             if (user) {
+//               token.id = user._id
+//             }
+//             return token
+//     }
     
 
 
-}
+// }
